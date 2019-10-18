e7277e560a6fd2a0e717de547bae733aded6b109
nishikiakinori <westakitaro@gmail.com>
2019-10-18 11:34:51 +0900

    10/18 課題

--- lib/enemy.rb
+++ lib/enemy.rb
@@ -1,1 +1,15 @@
 # 敵 (Enemy) のふるまいを表すクラス
+class Enemy
+  attr_reader :name, :hp
+
+  def initialize(name, hp)
+    @name = name
+    @hp = hp
+  end
+
+
+
+  def dead?
+    @hp <= 0
+  end
+end
--- lib/hero.rb
+++ lib/hero.rb
@@ -1,1 +1,23 @@
 # 勇者 (Hero) のふるまいを表すクラス
+class Hero
+  attr_reader :name, :hp
+
+  def initialize(name, hp)
+    @name = name
+    @hp = hp
+  end
+
+  def test_dead
+    hero_hp1 = Hero.new('ああああ', 1)
+    hero_hp0 = Hero.new('ああああ', 0)
+    hero_hpminus = Hero.new('ああああ', -1)
+
+    assert_equal(hero_hp1.dead?, false)
+    assert_equal(hero_hp0.dead?, true)
+    assert_equal(hero_hpminus.dead?, true)
+  end
+
+  def dead?
+    @hp <= 0
+  end
+end
